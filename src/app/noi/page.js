4a6be23b7d14694de4a7f87998c574da39d41930
Tommaso Coviello <tommaso.coviello2@gmail.com>
2023-10-18 20:50:36 +0200

    pages

---
+++ src/app/noi/page.js
@@ -0,0 +1,11 @@
+export default function Noi(){
+    return(
+        <div>
+            <h1>Noi</h1>
+            1 Nerd
+            1 Figo
+            1 Coglione
+            e poi c'è un mateoooooo
+        </div>
+    )
+}
---
+++ src/app/proposte/page.js
@@ -0,0 +1,8 @@
+export default function Proposte(){
+    return(
+        <div>
+            <h1>Proposte</h1>
+            Ciao mateoooooo
+        </div>
+    )
+}
